1e9d4e48d2059f21e7f0851f6229a1e408dd7c19
ay0469 <ay0469@192.168.7.26>
2014-10-04 12:24:39 +0200

    Añadido UserTest

---
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -0,0 +1,36 @@
+package es.upm.miw.spai.ecp2ecp2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+private User usr;
+
+@Before
+public void before() {
+    usr = new User(1,"David", "Perez");
+}
+
+
+@Test
+public void testUserIntStringString(){
+	assertEquals(1, usr.getNumber());
+    assertEquals("David", usr.getName());
+    assertEquals("Perez", usr.getFamilyName());
+}
+
+@Test
+public void testFullName(){
+	assertEquals("David Perez", usr.fullName());	
+}
+
+@Test
+public void testInitials(){
+	assertEquals("D.", usr.initials());
+	
+}
+
+
+}
