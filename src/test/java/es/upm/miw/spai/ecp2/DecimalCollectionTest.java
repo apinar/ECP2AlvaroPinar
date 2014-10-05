7f930d894404ce6ae3a4fee94609b5594b273f92
alvaro <alvaro@apinar>
2014-10-05 09:17:43 +0200

    Tests Implementados.

--- src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -1,5 +1,34 @@
 package es.upm.miw.spai.ecp2;
 
-public class DecimalCollectionTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+	private DecimalCollection dcol;
+	
+	@Before
+    public void before() {
+        dcol.add(1.5);
+        dcol.add(2.3);
+        dcol.add(2.0);
+    }
+
+	@Test
+	public void testSize() {
+		assertEquals(3, dcol.size());
+	}
+
+	@Test
+	public void testSum() {
+		assertEquals(5.8, dcol.sum(),0);		
+	}
+	
+	@Test
+	public void testHigher() {
+		assertEquals(2.3, dcol.higher(),0);
+	}
 }
+
+
--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -1,7 +1,36 @@
 package es.upm.miw.spai.ecp2;
 
-public class FractionTest {
-	//private Fraction fr;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class FractionTest {
+	private Fraction fr;
+
+	@Before
+	public void before() {
+	    fr = new Fraction(17, 5);
+	}
+	
+	@Test
+	public void testFractionIntInt() {
+        assertEquals(17, fr.getNumerator());
+        assertEquals(5, fr.getDenominator());
+    }
+	
+	@Test
+	public void testFraction() {
+        fr = new Fraction();
+        assertEquals(1, fr.getNumerator());
+        assertEquals(1, fr.getDenominator());
+    }
+	
+	
+	@Test
+	public void testDecimal() {
+        assertEquals(3.4, fr.decimal(),10^-2);
+    }
+	
 	
 }
